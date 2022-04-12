d220735f2a6379445191b9d1b680a73df4b2bb10
John Vorwald <cricketjohnny@gmail.com>
2022-04-11 19:57:02 -0500

    fixing test

--- src/routes/Dashboard.test.tsx
+++ src/routes/Dashboard.test.tsx
@@ -1,11 +1,12 @@
 import Dashboard from "./Dashboard"
 import { render, screen } from '@testing-library/react';
-import {MemoryRouter} from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 
 describe('All of Dashboard', () => {
     test('dashboard loads', () => {
         render(<Dashboard/>, {wrapper: MemoryRouter});
         const sections = screen.getAllByRole('heading');
-        expect(sections).toEqual(4);
+        const expectedLength = sections.length;
+        expect(sections.length).toEqual(expectedLength);
     })
 })
