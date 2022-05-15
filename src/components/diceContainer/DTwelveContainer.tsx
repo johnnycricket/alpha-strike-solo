58c79eef6d7923e010706cf070972e555edb700b
John Vorwald <cricketjohnny@gmail.com>
2022-05-14 20:39:33 -0500

    fleshing out the d12 container.

--- src/components/diceContainer/DTwelveContainer.tsx
+++ src/components/diceContainer/DTwelveContainer.tsx
@@ -1,21 +1,40 @@
+import { useState } from "react";
 
 interface D12Props {
     damageDice: String
 }
 
 const DTwelveContainer = (props:D12Props) => {
-    return(
+    const [isDynamic, setIsDynamic] = useState<Boolean>(true);
+    const [hasRolls, setHasRolls] = useState<Boolean>(false);
+    const [rolls, setRolls] = useState<Number[]>([]);
+    const [rollElements, setRollElements] = useState<HTMLElement[]>([]);
+
+    return (
         <div>
             <section>
                 <div>
                     <button>Roll</button>
-                    <form action="">
-                        <input type="number" name="" id="" />
+                    {isDynamic && <form>
+                        <input type="number" name="numberofdice" id="numberofdice" />
+                        <label htmlFor="numberofdice">Number of Dice</label>
+                    </form>}
+                    <button>Roll</button>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <h2>Damage Type</h2>
+                    <form>
+                        <input type="radio" name="dynamic" id="dynamic" />
+                        <label htmlFor="dynamic">Dynamic Damage</label>
+                        <input type="radio" name="one" id="one" />
+                        <label htmlFor="one">All In One</label>
                     </form>
                 </div>
             </section>
             <section>
-                
+                 {hasRolls && <div></div>}
             </section>
         </div>
     )
