0736e412e40f203746c85c272ce41a4930110c78
John Vorwald <cricketjohnny@gmail.com>
2023-03-16 18:47:44 -0500

    updated dependencies. fixed a type in the wrong case.

--- src/components/dieResult.tsx
+++ src/components/dieResult.tsx
@@ -2,29 +2,24 @@ import DTwelve from "./dice/DTwelve";
 import TwoDSix from "./dice/TwoDSix";
 
 interface ResultProps {
-    result: Number,
-    isD12?: boolean
-}
-
-const defaultProps: ResultProps = {
-    result: 0,
-    isD12: false
+    result: number;
+    isD12?: boolean;
 }
 
 const DieResult = (props: ResultProps) => {
     return (
         <div className="die-result">
-            {props.isD12 && 
+            {props.isD12 &&
                 <DTwelve/>
             }
             {!props.isD12 && 
                 <TwoDSix/>
             }
-            <span className="result-text">{props.result}</span>
+            <span className="result-text">
+                {props.result}
+            </span>
         </div>
     )
 }
-
-DieResult.defaultProps = defaultProps;
 
 export default DieResult;
